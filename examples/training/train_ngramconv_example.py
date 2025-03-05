f2214bb007670f7723862c1dbda200c5c5286550
danielgibert <danigibert24@gmail.com>
2025-03-05 09:46:13 +0100

    Removed old trainers and updated examples

--- examples/training/train_ngramconv_example.py
+++ examples/training/train_ngramconv_example.py
@@ -1,8 +1,9 @@
 from maltorch.zoo.ngramconv import NGramConv
 from maltorch.datasets.binary_dataset import BinaryDataset
 from torch.utils.data import DataLoader
-from maltorch.trainers.binary_trainer import BinaryTrainer
+from maltorch.trainers.early_stopping_pytorch_trainer import EarlyStoppingPyTorchTrainer
 import multiprocessing
+import torch
 
 ngramconv = NGramConv(out_channels=50)
 training_dataset = BinaryDataset(
@@ -32,7 +33,18 @@ validation_dataloader = DataLoader(
     num_workers=num_workers,
     collate_fn=training_dataset.pad_collate_func
 )
+criterion = torch.nn.BCELoss()
+optimizer = torch.optim.Adam(ngramconv.parameters())
 
-trainer = BinaryTrainer(num_epochs=5, patience=2, output_directory_path="models/ngramconv/")
-trainer.train(ngramconv, train_dataloader, validation_dataloader)
-
+trainer = EarlyStoppingPyTorchTrainer(
+    optimizer=optimizer,
+    epochs=5,
+    loss=criterion
+)
+model = trainer.train(
+    ngramconv,
+    train_dataloader,
+    validation_dataloader,
+    patience=2
+)
+torch.save(model.state_dict(), "models/ngramconv/model.pth")
--- examples/training/train_resnet18_example.py
+++ examples/training/train_resnet18_example.py
@@ -1,31 +1,49 @@
 from maltorch.zoo.resnet18 import ResNet18
 from maltorch.datasets.grayscale_dataset import GrayscaleDataset
 from torch.utils.data import DataLoader
-from secmlt.models.pytorch.base_pytorch_trainer import BasePyTorchTrainer
+from maltorch.trainers.early_stopping_pytorch_trainer import EarlyStoppingPyTorchTrainer
 import torch
 import multiprocessing
 
-resnet = ResNet18()
 
+resnet = ResNet18()
 training_dataset = GrayscaleDataset(
     goodware_directory="path/to/goodware/train/",
     malware_directory="path/to/malware/train/"
 )
 
+validation_dataset = GrayscaleDataset(
+    goodware_directory="path/to/goodware/train/",
+    malware_directory="path/to/malware/train/"
+)
 
 num_workers = max(multiprocessing.cpu_count() - 4, multiprocessing.cpu_count() // 2 + 1)
-dataloader = DataLoader(
+training_dataloader = DataLoader(
     training_dataset,
     batch_size=32,
     shuffle=True,
     num_workers=num_workers
 )
-loss = torch.nn.BCELoss()
-optimizer = torch.optim.Adam(resnet.parameters())
-trainer = BasePyTorchTrainer(
-    optimizer=optimizer,
-    epochs=1,
-    loss=loss,
+validation_dataloader = DataLoader(
+    training_dataset,
+    batch_size=32,
+    shuffle=True,
+    num_workers=num_workers
 )
-trainer.train(resnet, dataloader)
+criterion = torch.nn.BCELoss()
+optimizer = torch.optim.Adam(resnet.parameters())
+
+trainer = EarlyStoppingPyTorchTrainer(
+    optimizer=optimizer,
+    epochs=5,
+    loss=criterion
+)
+model = trainer.train(
+    resnet,
+    training_dataloader,
+    validation_dataloader,
+    patience=2
+)
+torch.save(model.state_dict(), "models/resnet18/model.pth")
+
 
