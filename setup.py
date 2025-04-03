c7f23a7d571331b6512cf9c23981aea7b2f4f1ad
zangobot <luca.demetrio@outlook.it>
2025-04-03 15:18:56 +0200

    Adding initial README with isntallation instructions. Adding version.

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
+import pathlib
+
 from setuptools import find_packages, setup
+
+here = pathlib.Path.cwd()
+readme_path = here / "README.md"
+version_path = here / "src" / "maltorch" / "VERSION"
+
+
+# Get the long description from the README file
+with readme_path.open() as f:
+    long_description = f.read()
+
+# Get the version from VERSION file
+with version_path.open() as f:
+    version = f.read()
+
 setup(
     name='maltorch',
-    version='0.1',
+    version=version,
     packages=find_packages(
         where="src",
         exclude=[
@@ -14,12 +30,18 @@ setup(
     package_dir={'': 'src'},
     url='',
     license='',
-    author='Luca Demetrio, Daniel Gibert, Andrea Ponte, Dmitrijs Trizna',
-    author_email='',
-    description='',
+    author='Luca Demetrio, Daniel Gibert, Andrea Ponte, Maura Pintor',
+    author_email='luca.demetrio@unige.it',
+    description='Pytorch-based library for creating Adversarial EXEmples against Windows Malware detectors.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
-        'torch', 'torchvision', 'scikit-learn', 'secml-torch', 'lightgbm==3.3.5', 'lief',
-        'git+https://github.com/zangobot/ember.git'
-        , 'nevergrad', 'joblib>=1.3.2'
+        'torch',
+        'torchvision',
+        'secml-torch',
+        'lightgbm==3.3.5',
+        'lief',
+        'nevergrad',
+        'joblib>=1.3.2'
     ],
 )
