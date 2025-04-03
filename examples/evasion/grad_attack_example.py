a085af681275feba40c6df6c56a6eaf7488d328c
zangobot <luca.demetrio@outlook.it>
2025-04-03 14:29:39 +0200

    Removed comments from examples

--- examples/evasion/grad_attack_example.py
+++ examples/evasion/grad_attack_example.py
@@ -1,12 +1,10 @@
 from pathlib import Path
 
 from secmlt.metrics.classification import Accuracy
-# from secmlt.trackers import TensorboardTracker, LossTracker
 from torch.utils.data import TensorDataset, DataLoader
 
 from maltorch.adv.evasion.partialdos import PartialDOS
 from maltorch.data.loader import load_from_folder, create_labels
-from maltorch.zoo.avaststyleconv import AvastStyleConv
 from maltorch.zoo.malconv import MalConv
 
 device = "cpu"
@@ -17,13 +15,12 @@ X = X.to(device)
 y = create_labels(X, 1)
 y = y.to(device)
 dl = DataLoader(TensorDataset(X, y), batch_size=3)
-# path = str(Path(__file__).parent / "logs" / "pdos")
-# tensorboard_tracker = TensorboardTracker(path, [LossTracker()])
+
 pdos_attack = PartialDOS(
     query_budget=20, trackers=None, random_init=False
 )
 model = MalConv.create_model(device=device)
-# avastconv = AvastStyleConv.create_model(device=device)
+
 print("Pre-attack accuracy: ", Accuracy()(model, dl))
 adv_dl = pdos_attack(model, dl)
 print("Accuracy: ", Accuracy()(model, adv_dl))
