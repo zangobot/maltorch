63e8a65d65f24953304974d1c4903997253dd439
zangobot <luca.demetrio@outlook.it>
2026-09-16 22:03:21 +0200

    Early stopping is working, fixing examples

--- examples/evasion/grad_attack_example.py
+++ examples/evasion/grad_attack_example.py
@@ -6,7 +6,6 @@ from secmlt.metrics.classification import Accuracy
 from secmlt.trackers import ScoresTracker
 from torch.utils.data import TensorDataset, DataLoader
 
-from maltorch.adv.evasion.base_optim_attack_creator import OptimizerBackends
 from maltorch.adv.evasion.partialdos import PartialDOS
 from maltorch.data.loader import load_from_folder, create_labels
 from maltorch.optim.halting import EarlyStopping
@@ -28,15 +27,13 @@ dl = DataLoader(TensorDataset(X, y), batch_size=1)
 
 tracker = ScoresTracker()
 
-query_budget = 100
-pdos_attack = PartialDOS(
+query_budget = 30
+
+grad_attack = PartialDOS(
     query_budget=query_budget,
-    trackers=None,
     random_init=False,
-    backend=OptimizerBackends.NG,
-    population_size=10,
     device=device,
-    early_stopping=EarlyStopping(20)
+    early_stopping=EarlyStopping(patience=6)
 )
 # Create the deep neural networks we want to evaluate.
 # All the parameters of the networks are fetched online, since we are not passing
@@ -52,5 +49,5 @@ for k in networks:
     print(k)
     model = networks[k]
     print("- - - Pre-attack accuracy: ", Accuracy()(model, dl))
-    adv_dl = pdos_attack(model, dl)
+    adv_dl = grad_attack(model, dl)
     print("- - - Accuracy: ", Accuracy()(model, adv_dl))
